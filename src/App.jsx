fc074b29b08f8e4fd4f6117b9956312dc877086d
Hitagi7 <ninkie123@gmail.com>
2024-04-20 17:28:36 +0800

    Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/layout/Header'
+
+function App() {
+
+  return (
+    <div className='App'>
+        <Header />
+        <hr />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ButtonStack.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './ButtonStack.css'
+
+function ButtonStack() {
+
+  return (
+    <div className='ButtonStack'>
+        <div className="Login">Log In</div>
+        <div className="Signup">Sign Up</div>
+    </div>
+  )
+}
+
+export default ButtonStack
