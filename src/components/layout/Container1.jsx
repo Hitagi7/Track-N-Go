f867a899e86b7d9e68e28bd3c897d44fa95655cb
Hitagi <92198361+Hitagi7@users.noreply.github.com>
2024-04-22 19:23:05 +0000

    style: updated css using flexbox

--- src/components/layout/Container1.jsx
+++ src/components/layout/Container1.jsx
@@ -4,32 +4,41 @@ import './Container1.css'
 function Container1() {
 
   return (
-    <div className='Container1'>
-        <img src="/src/assets/Map Pin.png" alt="Map With Pin" className="Resize-image1"/>
-        <div className="Text1">
-            <div className="Header1">
-              Effortlessly track your parcels
-            </div>
-            <div className="Description1">
-              Stay connected with your shipments every step of the way with Track N’ Go. Track your packages in real-time, receive timely updates on delivery status, and enjoy peace of mind knowing exactly where your parcels are.
-            </div>
-            <div className="Input">
-              <div className="InputTitle">
-                Track your parcel below!
-              </div>
-              <div className="InputSquare">
-                <div className="Wrapper">
-                  <img src="/src/assets/icon-search.svg" alt="icon-search" className='icon'/>
-                  <input type="text" className="InputBox" placeholder="Enter tracking number"/>
-                </div>
-                <button className="Search">
-                  Search
-                </button>
-              </div>
-            </div>
+    <div className="Container1">
+      <div className="Text1">
+        <div className="Header1">Effortlessly track your parcels</div>
+        <div className="Description1">
+          Stay connected with your shipments every step of the way with Track N'
+          Go. Track your packages in real-time, receive timely updates on
+          delivery status, and enjoy peace of mind knowing exactly where your
+          parcels are.
         </div>
+        <div className="Input">
+          <div className="InputTitle">Track your parcel below!</div>
+          <div className="InputSquare">
+            <div className="Wrapper">
+              <img
+                src="/src/assets/icon-search.svg"
+                alt="icon-search"
+                className="icon"
+              />
+              <input
+                type="text"
+                className="InputBox"
+                placeholder="Enter tracking number"
+              />
+            </div>
+            <button className="Search">Search</button>
+          </div>
+        </div>
+      </div>
+      <img
+          src="/src/assets/Map Pin.png"
+          alt="Map With Pin"
+          className="Resize-image1"
+        />
     </div>
-  )
+  );
 }
 
 export default Container1
