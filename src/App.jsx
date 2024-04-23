71fc7ef8a40c18ec4147709a28021614481857e4
Hitagi <92198361+Hitagi7@users.noreply.github.com>
2024-04-23 14:15:35 +0000

    feat: added container4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
-import Container1 from './components/layout/Container1'
-import Container2 from './components/layout/Container2'
-import Container3 from './components/layout/Container3'
+import Header from './components/landingpage/Header'
+import Footer from './components/landingpage/Footer'
+import Container1 from './components/landingpage/Container1'
+import Container2 from './components/landingpage/Container2'
+import Container3 from './components/landingpage/Container3'
+import Container4 from './components/landingpage/Container4'
 
 function App() {
 
@@ -12,9 +13,10 @@ function App() {
     <div className='App'>
         <Header />
         <hr />
-        <Container1/>
-        <Container2/>
-        <Container3/>
+        <Container1 />
+        <Container2 />
+        <Container3 />
+        <Container4 />
         <Footer />
     </div>
   )
