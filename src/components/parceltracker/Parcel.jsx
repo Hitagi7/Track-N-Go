8d3b65103a2e718e7237529903f419c87aa779c7
Hitagi <92198361+Hitagi7@users.noreply.github.com>
2024-04-25 10:11:09 +0000

    style: updated parcel tracker

---
+++ src/components/parceltracker/Parcel.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Parcel.css'
+
+function Parcel() {
+  return (
+    <div className='Parcel'>
+        <div className="information">
+            <div className="status">
+                <img src="/src/assets/icons/box-delivered.svg" alt="box-delivered"/>
+            </div>
+            <div className="refnumber">
+                #788667073435
+            </div>
+        </div>
+        <hr />
+        <div className="details">
+            <div className="weight">
+                <p className="font-14 font-semibold">14.6 kg</p>
+                <p className="font-10">Weight</p>
+            </div>
+            <div className="size">
+                <p className="font-14 font-semibold">21 x 60 cm</p>
+                <p className="font-10">Size</p>
+            </div>
+            <div className="price">
+                <p className="font-14 font-semibold">₱567</p>
+                <p className="font-10">Price</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Parcel
--- src/components/parceltracker/TrackedParcel.jsx
+++ src/components/parceltracker/TrackedParcel.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './TrackedParcel.css'
+import Parcel from './Parcel'
 
 function TrackedParcel() {
   return (
     <div className="TrackedParcel">
-        <p className="font-20 font-semibold">Tracked Parcel</p>
+      <p className="font-20 font-semibold">Tracked Parcel</p>
+      <Parcel />
     </div>
   )
 }
