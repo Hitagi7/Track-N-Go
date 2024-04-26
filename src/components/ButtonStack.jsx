920466ed810fbcdcaa3ae686841b7c710397de60
hanakki <109896839+hanakki@users.noreply.github.com>
2024-04-26 08:57:40 +0000

    feat: routed login and signup overlays with each other and from the landing page

--- src/components/ButtonStack.jsx
+++ src/components/ButtonStack.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './ButtonStack.css'
+import { useState } from 'react';
+import SignUpOverlay from './landingpage/SignupOverlay.jsx';
+import LoginOverlay from './landingpage/LoginOverlay.jsx';
+import './ButtonStack.css';
 
 function ButtonStack() {
+  const [loginVisible, setLoginVisible] = useState(false);
+  const [signupVisible, setSignupVisible] = useState(false);
+
+  const toggleLogin = () => {
+    setLoginVisible(!loginVisible);
+  };
+
+  const toggleSignup = () => {
+    setSignupVisible(!signupVisible);
+  };
+
+  const toggleLoginSignup = () => {
+    setLoginVisible(!loginVisible);
+    setSignupVisible(!signupVisible);
+  };
 
   return (
     <div className='ButtonStack'>
-        <div className="Login">Log In</div>
-        <div className="Signup">Sign Up</div>
+      <div className="Login" onClick={toggleLogin}>Log In</div>
+      <div className="Signup" onClick={toggleSignup}>Sign Up</div>
+
+      <SignUpOverlay visible={signupVisible} toggleVisible={toggleSignup} toggleLoginSignup={toggleLoginSignup} />
+      <LoginOverlay visible={loginVisible} toggleVisible={toggleLogin} toggleLoginSignup={toggleLoginSignup} />
     </div>
-  )
+  );
 }
 
-export default ButtonStack
+export default ButtonStack;
--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -5,10 +5,12 @@ import Container2 from '../landingpage/Container2'
 import Container3 from '../landingpage/Container3'
 import Container4 from '../landingpage/Container4'
 import Footer from '../landingpage/Footer'
-import Overlay from '../landingpage/Overlay'
+import SignupOverlay from '../landingpage/SignupOverlay'
+import LoginOverlay from '../landingpage/LoginOverlay'
 import './LandingPage.css'
 
 function LandingPage() {
+
   return (
     <div class="body">
       <Header />
@@ -17,7 +19,8 @@ function LandingPage() {
       <Container2 />
       <Container3 />
       <Container4 />
-      <Overlay />
+      <LoginOverlay />
+      <SignupOverlay />
       <Footer />
     </div>
   )
