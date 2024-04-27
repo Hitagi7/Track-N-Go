d9b49aa99624005ebf6ab161f95d2a1f86f69eb5
hanakki <109896839+hanakki@users.noreply.github.com>
2024-04-27 13:41:56 +0000

    feat: search parcel background image added

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -3,20 +3,33 @@ import './Settings.css'
 import SideNavigation from '../SideNavigation'
 import SettingsTitle from './SettingsTitle';
 
+// import IcBaselineFacebook from "../../../../assets/icons/icon-facebook";
+// import { IconSettings } from "src/assets/icons/icon-settings";
+// import { IconTrello } from "src/assets/icons/icon-dashboard";
+// import { IconUser } from "src/assets/icons/icon-Profile";
+// import { MailIcon } from "src/assets/icons/icon-mail";
+// import { MdiLinkedin } from "src/assets/icons/icon-linkedin";
+// import { PasswordIcon } from "src/assets/icons/icon-password";
+// import { RiInstagramFill } from "src/assets/icons/icon-instagram";
+// import { WfToggleItem } from "src/assets/icons/button-idle-toggle";
+
+
 function Settings() {
     return (
-      <div className="Settings">
+    <div className="Settings">
         <div className="Settings1">
-          <SideNavigation />
+        <SideNavigation />
         </div>
         <div className="Settings2">
-          <SettingsTitle />
-          <div className="Settings3"> {/* Main content/container */}
+        <SettingsTitle />
+        <div className="Settings3"> {/* Main content/container */}
             test
-          </div>
         </div>
-      </div>
+        </div>
+    </div>
     );
 }
+
+
 
 export default Settings
