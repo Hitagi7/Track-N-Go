63f73f1630a4cc5aee2c6a3fa84e549b47683bdf
Hitagi7 <20800091@usc.edu.ph>
2024-04-27 17:21:37 +0800

    feat: finished desgining ParcelTrackerBody (With placehodlers)

---
+++ src/components/landingpage/Overlay.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './Overlay.css'
+
+function Overlay() {
+
+    return (
+        <div class="overlay-container"> 
+            <div class="dimmed-background">
+            </div>
+            <div class="overlay">
+                <div class="signup overlay">
+                    <h3>Sign up to Track N' Go</h3>
+                    <div class="text-field">
+                        <p>Email</p>
+                        <form action="#">
+                            <img src="src/assets/icons/icon-mail.svg" alt="" class="mail-icon" />
+                            <input type="email" placeholder="john_doe@email.com" class="email-field" />
+                        </form>
+                    </div>
+                    <div class="text-field">
+                        <p>Full Name</p>
+                        <form action="#">
+                            <img src="src/assets/icons/icon-profile.svg" class="profile-icon" alt="" />
+                            <input type="text" placeholder="John Doe" class="name-field" />
+                        </form>
+                    </div>
+                    <div class="text-field">
+                        <p>Password</p>
+                        <form action="#">
+                            <img src="src/assets/icons/icon-password.svg" class="password-icon" alt="" />
+                            <input type="password" placeholder="Should be 8 characters and above" class="password-field" />
+                        </form>
+                    </div>
+                    <button class="fbutton primary">Create Account</button>
+                    <button class="fbutton secondary">Cancel</button>
+                    <div class="hyperlink">
+                        <p>Already have an account? <a href="#" class="hyperlink">Log in.</a> </p>
+                    </div>
+                </div>
+                <div class="login overlay">
+                    <h3>Log in to Track N' Go</h3>
+                    <div class="text-field">
+                        <p>Email</p>
+                        <form action="#">
+                            <img src="src/asset/icons/icon-mail.svg" alt="" class="mail-icon" />
+                            <input type="email" placeholder="john_doe@email.com" class="email-field" />
+                        </form>
+                    </div>
+                    <div class="text-field">
+                        <p>Password</p>
+                        <form action="#">
+                            <img src="src/asset/icons/icon-password.svg" class="password-icon" alt="" />
+                            <input type="password" placeholder="Should be 8 characters and above" class="password-field" />
+                        </form>
+                    </div>
+                    <button class="fbutton primary">Log in</button>
+                    <button class="fbutton secondary">Cancel</button>
+                    <div class="hyperlink">
+                        <p>Need an account? <a href="#" class="hyperlink">Create an account.</a> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Overlay
--- src/components/parceltracker/ParcelTrackerBody.jsx
+++ src/components/parceltracker/ParcelTrackerBody.jsx
@@ -4,6 +4,7 @@ import './ParcelTrackerBody.css'
 function ParcelTrackerBody() {
   return (
     <div className="ParcelTrackerBody">
+      {/* FRAME 1 */}
       <div className="frame1">
         <div className="city">
           <p className="font-24 font-bold">VALENZUELA CITY</p>
@@ -12,6 +13,8 @@ function ParcelTrackerBody() {
         </div>
         <div className="refNumber">SPEPH043036716934</div>
       </div>
+
+      {/* FRAME 2 */}
       <div className="frame2">
         <div className="order">
           <img src="/src/assets/icons/circle-active-order-created.svg" alt="circle-active-order-created" className='iconstatus'/>
@@ -38,6 +41,79 @@ function ParcelTrackerBody() {
         <p className='idle'>Delivered</p>
         </div>
       </div>
+
+    {/* FRAME 3 */}
+    <div className="frame3">
+
+      <div className="container">
+        <div className="left">
+          <div className="date">09 April 2024</div>
+          <div className="time">09:33</div>
+        </div>
+        <div className="middle">
+          <img src="/src/assets/icons/icon-circle-check.svg" alt="icon-circle-check" />
+          <div className="vertical-line"></div>
+        </div>
+        <div className="right">
+          <div className="description">[SOC 4] Your parcel is being transported to [SOC 5]</div>
+        </div>
+      </div>
+
+      <div className="container-placeholder-1">
+        <div className="left">
+          <div className="date">09 April 2024</div>
+          <div className="time">01:47</div>
+        </div>
+        <div className="middle">
+          <img src="/src/assets/icons/icon-circle.svg" alt="icon-circle" />
+          <div className="vertical-line"></div>
+        </div>
+        <div className="right">
+          <div className="description-1">[SOC 4] Your parcel has been received by sorting center</div>
+        </div>
+      </div>
+
+      <div className="container-placeholder-2">
+        <div className="left">
+          <div className="date">08 April 2024</div>
+          <div className="time">18:34</div>
+        </div>
+        <div className="middle">
+          <img src="/src/assets/icons/icon-circle.svg" alt="icon-circle" />
+          <div className="vertical-line"></div>
+        </div>
+        <div className="right">
+          <div className="description-2">[Paranaque DC] Your parcel has been picked up</div>
+        </div>
+      </div>
+
+      <div className="container-placeholder-3">
+        <div className="left">
+          <div className="date">08 April 2024</div>
+          <div className="time">18:09</div>
+        </div>
+        <div className="middle">
+          <img src="/src/assets/icons/icon-circle.svg" alt="icon-circle" />
+          <div className="vertical-line"></div>
+        </div>
+        <div className="right">
+          <div className="description-3">[SOC 4] Your parcel has been received by sorting center</div>
+        </div>
+      </div>
+
+      <div className="container-placeholder-4">
+        <div className="left">
+          <div className="date">07 April 2024</div>
+          <div className="time">22:59</div>
+        </div>
+        <div className="middle">
+          <img src="/src/assets/icons/icon-circle.svg" alt="icon-circle" />
+        </div>
+        <div className="right">
+          <div className="description-4">[SOC 4] Your parcel has been received by sorting center</div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
