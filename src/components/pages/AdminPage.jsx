3babcfb9df7fa7f8478162cbf277b956711d395e
Hitagi7 <20800091@usc.edu.ph>
2024-05-01 20:33:00 +0800

    feat: added admin page

---
+++ src/components/pages/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../landingpage/Header'
+
+function AdminPage() {
+  return (
+    <div className="AdminPage">
+        <Header />
+        <hr />
+        <p>test</p>
+    </div>
+  )
+}
+
+export default AdminPage
