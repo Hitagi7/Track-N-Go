616d56cde9213f20b89c1f362f56f3bee039e28a
Hitagi7 <ninkie123@gmail.com>
2024-05-02 18:19:46 +0800

    feat: added functionality on profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Profile.css";
+import { useState } from "react";
 import SideNavigation from "../SideNavigation";
 import ProfileTitle from "./ProfileTitle";
 import PublicProfile from "./PublicProfile";
 
+// Placeholder User
+const placeholderUser = {
+  firstName: "Marc",
+  lastName: "BNC",
+  userName: "I<3BNCS",
+  emailAddress: "BNC@gmail.com",
+};
+
 function Profile() {
+  const [user, setUser] = useState(placeholderUser);
+
+  const updateUser = (userUpdate) => {
+    setUser({ userUpdate });
+  };
+
   return (
     <div className="Profile">
       <div className="Profile1">
@@ -12,9 +27,10 @@ function Profile() {
       </div>
       <div className="Profile2">
         <ProfileTitle />
-        <div className="profile-header"/> {/* To be changed */}
-        <div className="Profile3"> {/* Main content/container */}
-            <PublicProfile />
+        <div className="profile-header" /> {/* To be changed */}
+        <div className="Profile3">
+          {/* Main content/container */}
+          <PublicProfile updateUser={updateUser} user={user} />
         </div>
       </div>
     </div>
--- src/components/profile/PublicProfile.jsx
+++ src/components/profile/PublicProfile.jsx
@@ -1,40 +1,129 @@
-import React from 'react'
-import './PublicProfile.css'
+import React from "react";
+import { useState } from "react";
+import "./PublicProfile.css";
 
-function PublicProfile() {
+function PublicProfile({ user, updateUser }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [firstNameBuffer, setFirstNameBuffer] = useState("");
+  const [lastNameBuffer, setLastNameBuffer] = useState("");
+  const [userNameBuffer, setUserNameBuffer] = useState("");
+  const [emailAddressBuffer, setEmailAddressBuffer] = useState("");
+
+  // Save changes
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      !firstNameBuffer &&
+      !lastNameBuffer &&
+      !userNameBuffer &&
+      !emailAddressBuffer
+    ) {
+      alert("Please input changes");
+      return;
+    }
+
+    const updatedUser = {
+      firstName: firstNameBuffer || user.firstName,
+      lastName: lastNameBuffer || user.lastName,
+      userName: userNameBuffer || user.userName,
+      emailAddress: emailAddressBuffer || user.emailAddress,
+    };
+
+    updateUser(updatedUser);
+
+    setFirstNameBuffer("");
+    setLastNameBuffer("");
+    setUserNameBuffer("");
+    setEmailAddressBuffer("");
+
+    console.log(user);
+  };
+
+  // Cancel changes
+  const onCancel = (e) => {
+    setFirstNameBuffer("");
+    setLastNameBuffer("");
+    setUserNameBuffer("");
+    setEmailAddressBuffer("");
+  };
+
   return (
     <div className="PublicProfile">
-        <div className="profile-frame">
-          <div className="title">Public profile</div>
-          <div className="profile-picture">
-            <img src="/src/assets/icons/profile-placeholder.svg" alt="profile-placeholder" className='profile-placeholder' />
-            <img src="/src/assets/icons/button-camera.svg" alt="button-camera" className='button-camera' />
+      <div className="profile-frame">
+        <div className="title">Public profile</div>
+        <div className="profile-picture">
+          <img
+            src="/src/assets/icons/profile-placeholder.svg"
+            alt="profile-placeholder"
+            className="profile-placeholder"
+          />
+          <img
+            src="/src/assets/icons/button-camera.svg"
+            alt="button-camera"
+            className="button-camera"
+          />
+        </div>
+        <div className="input-fields">
+          <div className="first-name">
+            <div className="input-title">First Name</div>
+            <input
+              type="text"
+              value={firstNameBuffer}
+              placeholder={firstName || user.firstName}
+              onChange={(e) => setFirstNameBuffer(e.target.value)}
+              className="input-box"
+            />
           </div>
-          <div className="input-fields">
-            <div className="first-name">
-              <div className="input-title">First Name</div>
-              <input type="text" placeholder='Juan' className='input-box' />
-            </div>
-            <div className="last-name">
-              <div className="input-title">Last Name</div>
-              <input type="text" placeholder='Dela Cruz' className='input-box' />
-            </div>
-            <div className="username">
-              <div className="input-title" >Username</div>
-              <input type="text" placeholder='@juan_delacruz' className='input-box' />
-            </div>
-            <div className="email-address">
-              <div className="input-title">Email Address</div>
-              <input type="text" placeholder='juan_delacruz@gmail.com' className='input-box' />
-            </div>
+          <div className="last-name">
+            <div className="input-title">Last Name</div>
+            <input
+              type="text"
+              value={lastNameBuffer}
+              placeholder={lastName || user.lastName}
+              onChange={(e) => setLastNameBuffer(e.target.value)}
+              className="input-box"
+            />
           </div>
-          <div className="profile-buttons">
-            <button className="save-changes-profile">Save changes</button>
-            <button className="cancel-profile">Cancel</button>
+          <div className="username">
+            <div className="input-title">Username</div>
+            <input
+              type="text"
+              value={userNameBuffer}
+              placeholder={
+                user.userName !== ""
+                  ? `@${user.userName}`
+                  : "@username" || userName
+              }
+              onChange={(e) => setUserNameBuffer(e.target.value)}
+              className="input-box"
+            />
+          </div>
+          <div className="email-address">
+            <div className="input-title">Email Address</div>
+            <input
+              type="text"
+              value={emailAddressBuffer}
+              placeholder={emailAddress || user.emailAddress}
+              onChange={(e) => setEmailAddressBuffer(e.target.value)}
+              className="input-box"
+            />
           </div>
         </div>
+        <div className="profile-buttons">
+          <button className="save-changes-profile" onClick={onSubmit}>
+            Save changes
+          </button>
+          <button className="cancel-profile" onClick={onCancel}>
+            Cancel
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PublicProfile
+export default PublicProfile;
