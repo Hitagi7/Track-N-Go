b6039fe0899e85f350220f58dbb7478af0a904e3
Yogi <113133628+YogiYoink@users.noreply.github.com>
2024-05-02 09:36:42 +0000

    feat: added admin parcel page features

--- src/components/admin/AdminParcelsPage/ParcelsAdmin.jsx
+++ src/components/admin/AdminParcelsPage/ParcelsAdmin.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import AdminSideBar from '../AdminSideBar'
+import AdminParcelBody from './AdminParcelBody'
+import './ParcelsAdmin.css'
 
 const ParcelsAdmin = () => {
   return (
-    <AdminSideBar/>
+    <div className="admin-homepage">
+            <div className="AdminBodySideNavigation">
+                <AdminSideBar />
+            </div>
+            <div className="AdminParcelBody">
+              <p	className="ParcelAdminHeader">Parcels - Admin View</p>
+              <AdminParcelBody />
+            </div>
+        </div>
 )}
 
 export default ParcelsAdmin;
