2122fa238f1a3f328a4a67e38f3a5aaf6e33c835
Yogi <113133628+YogiYoink@users.noreply.github.com>
2024-05-03 06:34:30 +0000

    feat: finished the admin tacking page

--- src/components/admin/AdminTrackingPage/AdminTrackingBody.jsx
+++ src/components/admin/AdminTrackingPage/AdminTrackingBody.jsx
@@ -26,22 +26,20 @@ function AdminTrackingBody() {
                     <tr class="AdminTrackingHeader">
                         <th> </th>
                         <th>TRACKING ID</th>
-                        <th>WEIGHT</th>
-                        <th>SIZE</th>
-                        <th>PRICE</th>
+                        <th>SHIPPED OUT</th>
+                        <th>DELIVERY DATE</th>
+                        <th>DELIVERED FROM</th>
+                        <th>DELIVERED TO</th>
                         <th>STATUS</th>
-                        <th>SHIPPER</th>
-                        <th>COURIER</th>
                     </tr>
-                    <tr class="AdminParcelTable">
+                    <tr class="AdminTrackingTable">
                         <td><img src="src/assets/icons/icon-edit.svg" alt="edit" class="ParcelEditIcon"/></td>
                         <td>NDJ297ADM1EL</td>
-                        <td>2.1 kg</td>
-                        <td>14 x 14 x 2 cm</td>
-                        <td>₱178.00</td>
-                        <td>Nat’l Bookstore</td>
-                        <td>J&T</td>
-                        <td>In transit</td>
+                        <td>2024/03/14</td>
+                        <td>2024/03/21</td>
+                        <td>Laguna Del Norte, PH</td>
+                        <td>Cebu City, PH</td>
+                        <td>[2] Picked Up</td>
                     </tr>
                 </table>
             </div>
