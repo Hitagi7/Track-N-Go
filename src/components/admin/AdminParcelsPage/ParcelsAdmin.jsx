9e840c8fd9c7e40233ea7ab7ce4a9786ca4120b0
Hitagi <92198361+Hitagi7@users.noreply.github.com>
2024-05-06 22:25:54 +0800

    docs: removed placeholder and placed reminder in ParcelsAdmin

--- src/components/admin/AdminParcelsPage/ParcelsAdmin.jsx
+++ src/components/admin/AdminParcelsPage/ParcelsAdmin.jsx
@@ -4,28 +4,8 @@ import AdminParcelBody from "./AdminParcelBody";
 import { useState } from "react";
 import "./ParcelsAdmin.css";
 
-const parcelPlaceHolder = [
-  {
-    id: "NDJ297ADM1EL",
-    weight: 2.1,
-    size: "14 x 14 x 2",
-    price: 178.0,
-    shipper: "Nat’l Bookstore",
-    courier: "J&T",
-    status: "In transit",
-  },
-  {
-    id: "ERYEE2342H246",
-    weight: 5.2,
-    size: "20 x 20 x 10",
-    price: 362.0,
-    shipper: "Data Blitz",
-    courier: "Lalamove",
-    status: "In transit",
-  },
-];
-
 const ParcelsAdmin = () => {
+  // Add another file to store the parcel details, so that both parcel and tracking can access the data
   const [parcelDetails, setParcelDetails] = useState([]);
 
   // Add parcel
